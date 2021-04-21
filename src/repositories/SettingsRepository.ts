a04420d11d6324f0863f31610383c0b05c612e8f
perseul <perseulmeira@gmail.com>
2021-04-21 11:40:05 -0300

    Terceiro dia

--- src/repositories/SettingsRepository.ts
+++ src/repositories/SettingsRepository.ts
@@ -1,5 +1,4 @@
-import { Repository, EntityRepository } from 'typeorm';
-
+import { EntityRepository, Repository } from 'typeorm';
 import { Setting } from '../entities/Setting';
 
 @EntityRepository(Setting)
---
+++ src/services/UsersService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+class UsersService {
+    private usersRepository: Repository<User>
+
+    constructor() {
+      this.usersRepository = getCustomRepository(UsersRepository);
+    }
+    async create(email: string) {
+      //Verificar se o usuário existe
+      
+      const userExists = await this.usersRepository.findOne({
+        email
+      });
+
+      //Se existir, retornar user
+      if(userExists){
+          return userExists;
+      }
+
+      const user = this.usersRepository.create ({
+          email
+      });
+
+      await this.usersRepository.save(user);
+        
+      //Senão existir, salvar no DB
+      return user;
+        
+    }
+}
+
+export { UsersService };
